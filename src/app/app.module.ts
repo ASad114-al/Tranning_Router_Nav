78c46a60926ab80a31d195d219d1f6e164db07d7
Asad <ali@rising-systems.de>
2022-01-10 20:27:54 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmploywwListComponent } from './employww-list/employww-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DepartmentListComponent, EmploywwListComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
