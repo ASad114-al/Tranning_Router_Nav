c19f40303b7eb99e454db6e1f0f8b1babb0601ad
Asad <ali@rising-systems.de>
2022-01-10 20:26:15 +0100

    create new project Tranning

---
+++ src/app/employww-list/employww-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employww-list',
+  templateUrl: './employww-list.component.html',
+  styleUrls: ['./employww-list.component.css']
+})
+export class EmploywwListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
